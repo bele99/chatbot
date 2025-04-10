485058a7c8c6996c600ea021c9f4e99b536d078d
Bella LIU DUSKE <100842214+bele99@users.noreply.github.com>
2025-04-10 21:39:09 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ if st.button("✨ 生成八字分析"):
         st.json(bazi_result)
 
         # AI 分析内容
-        prompt = f"请根据以下八字信息为用户生成命理分析：binbin"# "\n{bazi_result}"
+        prompt = f"请根据以下八字信息为用户生成命理分析：\n{bazi_result}"
         openai.api_key = st.secrets["api_key"]
         #openai.api_key = st.secrets["openai"]["api_key"]
 
@@ -79,7 +79,8 @@ if st.button("✨ 生成八字分析"):
             response = openai.completions.create(
                 model="gpt-3.5-turbo-instruct", 
                 prompt=prompt,
-                max_tokens=100
+                max_tokens=7,
+                temperature=0
             )
             ai_response = response['choices'][0]['text']
         except openai.OpenAIError as e:
